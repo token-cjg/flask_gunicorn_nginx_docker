2271bad261f83dcc143599cba3aa22c5703d5e99
Christopher Goddard <chrisjgoddard@gmail.com>
2021-04-30 12:25:04 +1000

    Wip

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,10 +1,3 @@
-from flask import Flask
-from flask import request, jsonify
-from random import sample
-import subprocess
-
-server = Flask(__name__)
-
 def run_request():
     index = int(request.json['index'])
     list = ['red', 'green', 'blue', 'yellow', 'black']
@@ -13,10 +6,27 @@ def run_request():
 def run_faas_request():
     function = request.json['function']
     # ref xspdf.com/resolution/50942456.html
-    process = subprocess.Popen(['export', 'OPENFAAS_URL=https://faasd.cthulu.tk', '&&', 'echo', '|', 'faas-cli', 'invoke', function], stdout=subprocess.PIPE)
-    stdout = process.communicate()[0]
-    return stdout
+    # cmd = "/bin/sh -c export OPENFAAS_URL=https://faasd.cthulu.tk && echo | faas-cli invoke catapi"
+    cmd2 = "export OPENFAAS_URL=https://faasd.cthulu.tk && echo | faas-cli invoke catapi"
+    args = shlex.split(cmd2)
+    #proc = Popen(args, stdout=PIPE, stderr=PIPE)
+    proc = Popen(args, shell=True, stdout=PIPE, stderr=PIPE)
+    out, err = proc.communicate()
+    exitcode = proc.returncode
+    return out
 
+def run_faas_request2():
+    cmd = "export OPENFAAS_URL=https://faasd.cthulu.tk && echo | faas-cli invoke catapi"
+    args = shlex.split(cmd)
+    success = False
+    while success = False:
+        try:
+            output = subprocess.check_output(args, stderr=subprocess.STDOUT).decode()
+            success = True
+        except subprocess.CalledProcessError as e:
+            output = e.output.decode()
+            success = False
+    return out.decode()
 
 @server.route('/', methods=['GET', 'POST'])
 def hello_world():
@@ -31,3 +41,62 @@ def make_faas_request():
         return 'This is the faas route.  Send a POST request with json payload of the form { function: catapi }.'
     if request.method == 'POST':
         return run_faas_request()
+
+@server.route('/faas2', methods=['GET', 'POST'])
+def make_faas_request2():
+    if request.method == 'GET':
+        return 'This is the faas route.  Send a POST request with json payload of the form { function: catapi }.'
+    if request.method == 'POST':
+        return run_faas_request2()
+"flask_app/app.py" 58L, 2069C                                                                       54,22         Bot
+
+def run_request():
+    index = int(request.json['index'])
+    list = ['red', 'green', 'blue', 'yellow', 'black']
+    return list[index]
+
+def run_faas_request():
+    function = request.json['function']
+    # ref xspdf.com/resolution/50942456.html
+    # cmd = "/bin/sh -c export OPENFAAS_URL=https://faasd.cthulu.tk && echo | faas-cli invoke catapi"
+    cmd2 = "export OPENFAAS_URL=https://faasd.cthulu.tk && echo | faas-cli invoke catapi"
+    args = shlex.split(cmd2)
+    #proc = Popen(args, stdout=PIPE, stderr=PIPE)
+    proc = Popen(args, shell=True, stdout=PIPE, stderr=PIPE)
+    out, err = proc.communicate()
+    exitcode = proc.returncode
+    return out
+
+def run_faas_request2():
+    cmd = "export OPENFAAS_URL=https://faasd.cthulu.tk && echo | faas-cli invoke catapi"
+    args = shlex.split(cmd)
+    success = False
+    while success = False:
+        try:
+            output = subprocess.check_output(args, stderr=subprocess.STDOUT).decode()
+            success = True
+        except subprocess.CalledProcessError as e:
+            output = e.output.decode()
+            success = False
+    return out.decode()
+
+@server.route('/', methods=['GET', 'POST'])
+def hello_world():
+    if request.method == 'GET':
+        return 'The model is up and running. Send a POST request'
+    else:
+        return run_request()
+
+@server.route('/faas', methods=['GET', 'POST'])
+def make_faas_request():
+    if request.method == 'GET':
+        return 'This is the faas route.  Send a POST request with json payload of the form { function: catapi }.'
+    if request.method == 'POST':
+        return run_faas_request()
+
+@server.route('/faas2', methods=['GET', 'POST'])
+def make_faas_request2():
+    if request.method == 'GET':
+        return 'This is the faas route.  Send a POST request with json payload of the form { function: catapi }.'
+    if request.method == 'POST':
+        return run_faas_request2()
